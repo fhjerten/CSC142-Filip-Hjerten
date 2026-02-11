bdff60ba9abc3b0b7f63538c8a92e93044e21d79
Filip Hjerten <f.hjerten@gmail.com>
2026-02-11 17:59:23 +0100

    update

---
+++ CSC142-VibeCode1/player.py
@@ -0,0 +1,18 @@
+class Player:
+
+    def __init__(self, team, gender, number, name, stats):
+        self.team = team
+        self.gender = gender
+        self.number = int(number)
+        self.name = name
+        self.stats = stats
+
+    def __str__(self):
+        return f"{self.number} {self.name}"
+    
+    def display_stats(self):
+        print ("stats for", self.name, "number", self.number)
+        for stat, value in self.stats.items():
+            print (f"{stat}: {value}")
+
+ 
